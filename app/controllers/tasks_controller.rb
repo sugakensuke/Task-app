f1f13db37836caf77379ae4f053106b50b0a784b
EC2 Default User <ec2-user@ip-172-31-35-29.ap-northeast-1.compute.internal>
2020-04-18 09:52:23 +0000

    「5. 4. 3　renderメソッドを使おう」まで済み

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,4 +19,28 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
   
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    @task.name = params[:name]
+    if @task.save
+      redirect_to user_tasks_url
+    else
+      render :edit
+    end
+  end
+
+  
+
+
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to user_tasks_url
+  end
+  
 end
